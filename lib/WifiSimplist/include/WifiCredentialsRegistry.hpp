097b80d69dca4794cf9fac3b69ff2f8f24ddf851
David SPORN <sporniket.studio@gmail.com>
2023-06-02 06:53:49 +0200

    solidify api of wifi credentials registry

--- lib/WifiSimplist/include/WifiCredentialsRegistry.hpp
+++ lib/WifiSimplist/include/WifiCredentialsRegistry.hpp
@@ -20,13 +20,55 @@ private:
   /**
    * @brief the internal storage of the registry.
    */
-  std::vector<WifiCredentials> registry;
+  std::vector<WifiCredentials *> registry;
+
+  std::vector<WifiCredentials *>::iterator registryIterator;
   /**
    * @brief The registry will have a limited size, the default value will be
    * `DEFAULT_MAX_SIZE`.
    *
    */
   uint8_t maxSize;
+  // Utilities
+  /**
+   * @brief Find an entry matching the query (same SSID) and return it.
+   *
+   * @param query the entry to find.
+   * @return WifiCredentials* if there is no entry, returns `nullptr`.
+   */
+  WifiCredentials *find(WifiCredentials *query);
+  /**
+   * @brief Rank of each entry except the provided one is incremented.
+   *
+   * @param query the entry to keep untouched.
+   */
+  void moveRankUpExcept(WifiCredentials *query);
+  /**
+   * @brief Rank of each entry from the first to the provided one (excluded) is incremented.
+   * 
+   * @param query the entry from which to stop.
+   */
+  void moveRankUpUntil(WifiCredentials *query);
+  /**
+   * @brief Rank of each entry starting from the provided one (excluded) is decremented.
+   * 
+   * @param query 
+   */
+  void moveRankDownAfter(WifiCredentials *query);
+  /**
+   * @brief Sort the entries by rank, and regenerate the enumeration.
+   */
+  void rearrange() ;
+  /**
+   * @brief Comparator to sort the entries by rank.
+   * 
+   * @param e1 the left hand side
+   * @param e2 the right hand side.
+   * @return true if the left hand side is before the right hand side.
+   */
+  static bool compareEntriesByRank(WifiCredentials* e1, WifiCredentials* e2) {
+    return e1->rank < e2->rank ;
+  }
 
 public:
   WifiCredentialsRegistry(
@@ -37,26 +79,59 @@ public:
   // enumeration interface.
   /**
    * @brief Enumeration interface -- go back to the begining of the sequence.
-   * 
-   * @return WifiCredentialsRegistry* this registry, to be able to chain with another call.
+   *
+   * @return WifiCredentialsRegistry* this registry, to be able to chain with
+   * another call.
    */
-  WifiCredentialsRegistry* rewind() ;
+  WifiCredentialsRegistry *rewind() {
+    registryIterator = registry.begin();
+    return this;
+  }
   /**
    * @brief Enumeration interface -- ask whether there will be another element.
-   * 
+   *
    * @return true when call to `next()` will not return `null`
    */
-  bool hasNext() ;
+  bool hasNext() { return registryIterator != registry.end(); }
   /**
    * @brief Enumeration interface -- get the next element.
-   * 
-   * @return WifiCredentials* the next element, or `null` if the enumeration is finished.
+   *
+   * @return WifiCredentials* the next element, or `nullptr` if the enumeration
+   * is finished.
    */
-  WifiCredentials* next() ;
+  WifiCredentials *const next() {
+    WifiCredentials *result = *registryIterator;
+    registryIterator++;
+    return result;
+  }
   // update interface
-  WifiCredentialsRegistry*  put(WifiCredentials *credentials) ;
-  WifiCredentialsRegistry*  remove(WifiCredentials *credentials) ;
-  WifiCredentialsRegistry*  setPreferred(WifiCredentials *credentials) ;
+  /**
+   * @brief Registers new credentials, or an update of existing credentials
+   * (same SSID). If the registry has more entries than the maximum size, the
+   * entry with the highest rank is removed. The rank of the new/updated entry
+   * will be 0 (first).
+   *
+   * @param credentials the credentials to register, will be deep-cloned.
+   * @return WifiCredentialsRegistry* this registry, to be able to chain with
+   * another call.
+   */
+  WifiCredentialsRegistry *put(WifiCredentials *const credentials);
+  /**
+   * @brief Remove the credentials (same SSID) from the registry.
+   *
+   * @param credentials the credentials to remove.
+   * @return WifiCredentialsRegistry* this registry, to be able to chain with
+   * another call.
+   */
+  WifiCredentialsRegistry *remove(WifiCredentials *const credentials);
+  /**
+   * @brief The given credentials, if found in the registry (same SSID) becomes
+   * the first to be given after a call to `rewind`.
+   *
+   * @param credentials the credential to reprioritize.
+   * @return WifiCredentialsRegistry*
+   */
+  WifiCredentialsRegistry *setPreferred(WifiCredentials *const credentials);
 };
 
 #endif
--- lib/WifiSimplist/src/WifiCredentialsRegistry.cpp
+++ lib/WifiSimplist/src/WifiCredentialsRegistry.cpp
@@ -2,6 +2,112 @@
 // header include
 #include "WifiCredentialsRegistry.hpp"
 
-WifiCredentialsRegistry::~WifiCredentialsRegistry(){}
+WifiCredentialsRegistry::~WifiCredentialsRegistry() {}
 // write code here...
 
+WifiCredentials *WifiCredentialsRegistry::find(WifiCredentials *query) {
+  for (std::vector<WifiCredentials *>::iterator it = registry.begin();
+       it != registry.end(); it++) {
+    WifiCredentials *candidate = *it;
+    if (std::equal(std::begin(candidate->ssid), std::end(candidate->ssid),
+                   std::begin(query->ssid), std::end(query->ssid))) {
+      return candidate;
+    }
+  }
+  return nullptr;
+}
+
+void WifiCredentialsRegistry::moveRankUpExcept(WifiCredentials *query) {
+  for (std::vector<WifiCredentials *>::iterator it = registry.begin();
+       it != registry.end(); it++) {
+    WifiCredentials *entry = *it;
+    if (entry != query) {
+      entry->rank++;
+    }
+  }
+}
+
+void WifiCredentialsRegistry::moveRankUpUntil(WifiCredentials *query) {
+  for (std::vector<WifiCredentials *>::iterator it = registry.begin();
+       it != registry.end(); it++) {
+    WifiCredentials *entry = *it;
+    if (entry == query) {
+      break;
+    }
+    entry->rank++;
+  }
+}
+
+void WifiCredentialsRegistry::moveRankDownAfter(WifiCredentials *query) {
+  bool found = false;
+  for (std::vector<WifiCredentials *>::iterator it = registry.begin();
+       it != registry.end(); it++) {
+    WifiCredentials *entry = *it;
+    if (entry == query) {
+      found = true;
+      continue;
+    }
+    if (found) {
+      entry->rank--;
+    }
+  }
+}
+
+void WifiCredentialsRegistry::rearrange() {
+  std::sort(registry.begin(), registry.end(), compareEntriesByRank);
+  rewind();
+}
+
+WifiCredentialsRegistry *
+WifiCredentialsRegistry::put(WifiCredentials *const credentials) {
+  WifiCredentials *entry = find(credentials);
+  if (nullptr == entry) {
+    // insert
+    entry = new WifiCredentials();
+    std::copy(std::begin(credentials->ssid), std::end(credentials->ssid),
+              std::begin(entry->ssid));
+    std::copy(std::begin(credentials->key), std::end(credentials->key),
+              std::begin(entry->key));
+    entry->keyType = credentials->keyType;
+    entry->rank = 0;
+    registry.push_back(entry);
+
+  } else {
+    // update
+    entry->keyType = credentials->keyType;
+    std::copy(std::begin(credentials->key), std::end(credentials->key),
+              std::begin(entry->key));
+    entry->rank = 0;
+  }
+  moveRankUpUntil(entry);
+  rearrange();
+  return this;
+}
+
+WifiCredentialsRegistry *
+WifiCredentialsRegistry::remove(WifiCredentials *const credentials) {
+  WifiCredentials *entry;
+  std::vector<WifiCredentials *>::iterator it = registry.begin();
+  while (it != registry.end()) {
+    entry = *it;
+    if (std::equal(std::begin(entry->ssid), std::end(entry->ssid),
+                   std::begin(credentials->ssid),
+                   std::end(credentials->ssid))) {
+      moveRankDownAfter(entry);
+      registry.erase(it);
+      return rewind();
+    }
+    it++;
+  }
+  return this;
+}
+
+WifiCredentialsRegistry *
+WifiCredentialsRegistry::setPreferred(WifiCredentials *const credentials) {
+  WifiCredentials *entry = find(credentials);
+  if (nullptr != entry) {
+    moveRankUpUntil(entry);
+    rearrange();
+  }
+  return this;
+}
